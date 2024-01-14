1187dabfc2a8d3f7f6bbe027687175b5bd161914
Riggor-Morthis <56031976+Riggor-Morthis@users.noreply.github.com>
2024-01-14 13:43:46 +0100

    Working greyscale conversion

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,22 @@
 package main;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+
+import picture.PixelConverter;
+
 public class Main {
 
-	public static void main(String[] args) {
-
+	static Random rand;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedImage test = PixelConverter.ConvertPicture("pictures/test.png", 256);
+		
+		File outputFile = new File("pictures/testGreyscale.png");
+		ImageIO.write(test, "png", outputFile);
 	}
-
 }
