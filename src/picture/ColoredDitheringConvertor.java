7e19b4e55943b2ea64b15fd87fec49a96dfbc324
Riggor-Morthis <roc.syl@outlook.com>
2024-01-16 09:43:23 +0100

    Working colored pixel-art

--- src/picture/ColoredDitheringConvertor.java
+++ src/picture/ColoredDitheringConvertor.java
@@ -8,13 +8,11 @@ public final class ColoredDitheringConvertor {
 	/* VARIABLES */
 
 	private static BufferedImage _exportedPicture;
-	private static BufferedImage _coloredPicture;
-	private static BufferedImage _grayscaledPicture;
+	private static BufferedImage _importedPicture;
 
 	private static int _pictureWidth, _pictureHeight;
 
-	private static int _whitePixel, _lightWhitePixel, _lightBlackPixel, _mediumWhitePixel, _mediumBlackPixel,
-			_darkWhitePixel, _darkBlackPixel, _blackPixel;
+	private static int _whitePixel, _lightPixel, _mediumPixel, _darkPixel, _blackPixel;
 
 	/* CONSTRUCTORS */
 
@@ -25,133 +23,68 @@ public final class ColoredDitheringConvertor {
 	/* PUBLIC METHODS */
 
 	public static BufferedImage convertPicture(String filepath, int exportedWidth) {
-		_coloredPicture = PictureImporter.importPicture(filepath);
-		_grayscaledPicture = PictureImporter.importPicture(filepath);
+		_importedPicture = PictureImporter.importPicture(filepath);
 		return executeConversion(exportedWidth);
 	}
 
 	public static BufferedImage convertPicture(BufferedImage importedPicture, int exportedWidth) {
-		_coloredPicture = importedPicture;
-		_grayscaledPicture = importedPicture;
+		_importedPicture = importedPicture;
 		return executeConversion(exportedWidth);
 	}
 
 	/* PRIVATE METHODS */
 
 	private static BufferedImage executeConversion(int exportedWidth) {
-		preparePictures(exportedWidth);
-		prepareVariables();
+		prepareVariables(exportedWidth);
 		getPixelShades();
+		mainLoop();
 
 		return _exportedPicture;
 	}
 
-	private static void preparePictures(int exportedWidth) {
-		_coloredPicture = PixelArtConvertor.convertPicture(_coloredPicture, exportedWidth);
+	private static void prepareVariables(int exportedWidth) {
+		_importedPicture = PixelArtConvertor.convertPicture(_importedPicture, exportedWidth);
 
-		_grayscaledPicture = GrayscaleConvertor.convertPicture(_coloredPicture);
-		_grayscaledPicture = PixelArtConvertor.convertPicture(_grayscaledPicture, exportedWidth);
-	}
-
-	private static void prepareVariables() {
-		_pictureWidth = _grayscaledPicture.getWidth();
-		_pictureHeight = _grayscaledPicture.getHeight();
+		_pictureWidth = _importedPicture.getWidth();
+		_pictureHeight = _importedPicture.getHeight();
 
 		_exportedPicture = new BufferedImage(_pictureWidth, _pictureHeight, BufferedImage.TYPE_INT_ARGB);
 	}
 
 	private static void getPixelShades() {
-		int[] twoValues;
-		BiPredicate<Integer, Integer> ditheringPattern;
-
 		_blackPixel = extractLocationBasedColours(0);
-		System.out.println(_blackPixel);
-
-		ditheringPattern = (x, y) -> (x % 2 == 0 || y % 2 == 0);
-		twoValues = extractLocationBasedColours(1, ditheringPattern);
-		_lightBlackPixel = twoValues[0];
-		_lightWhitePixel = twoValues[1];
-		System.out.println(_lightWhitePixel + " " + _lightBlackPixel);
-
-		ditheringPattern = (x, y) -> ((x + y) % 2 == 0);
-		twoValues = extractLocationBasedColours(1, ditheringPattern);
-		_mediumBlackPixel = twoValues[0];
-		_mediumWhitePixel = twoValues[1];
-		System.out.println(_mediumWhitePixel + " " + _mediumBlackPixel);
-
-		ditheringPattern = (x, y) -> (x % 2 == 0 && y % 2 == 0);
-		twoValues = extractLocationBasedColours(1, ditheringPattern);
-		_darkBlackPixel = twoValues[0];
-		_darkWhitePixel = twoValues[1];
-		System.out.println(_darkWhitePixel + " " + _darkBlackPixel);
-
+		_lightPixel = extractLocationBasedColours(1);
+		_mediumPixel = extractLocationBasedColours(2);
+		_darkPixel = extractLocationBasedColours(3);
 		_whitePixel = extractLocationBasedColours(4);
-		System.out.println(_whitePixel);
-	}
-
-	private static int[] extractLocationBasedColours(int targetRange, BiPredicate<Integer, Integer> ditheringPattern) {
-		int[] lightColour = new int[5];
-		int[] darkColour = new int[5];
-		int pixel;
-
-		for (int x = 0; x < _pictureWidth; x++) {
-			for (int y = 0; y < _pictureHeight; y++) {
-				pixel = _grayscaledPicture.getRGB(x, y);
-
-				if (pixelToLuminosity(pixel) / 51 == targetRange) {
-					if (ditheringPattern.test(x, y)) {
-						darkColour[0] += (pixel >> 24) & 0xff;
-						darkColour[1] += (pixel >> 16) & 0xff;
-						darkColour[2] += (pixel >> 8) & 0xff;
-						darkColour[3] += pixel & 0xff;
-						darkColour[4]++;
-					} else {
-						lightColour[0] += (pixel >> 24) & 0xff;
-						lightColour[1] += (pixel >> 16) & 0xff;
-						lightColour[2] += (pixel >> 8) & 0xff;
-						lightColour[3] += pixel & 0xff;
-						lightColour[4]++;
-					}
-				}
-			}
-		}
-		
-		for (int i = 0; i < 4; i++) {
-			lightColour[i] /= lightColour[4];
-			darkColour[i] /= darkColour[4];
-		}
-
-		int lightPixel = (255 << 24) | (lightColour[1] << 16) | (lightColour[2] << 8) | lightColour[3];
-		int darkPixel = (255 << 24) | (darkColour[1] << 16) | (darkColour[2] << 8) | darkColour[3];
-
-		return new int[] { darkPixel, lightPixel };
 	}
 
 	private static int extractLocationBasedColours(int targetRange) {
-		int[] lightColour = new int[5];
-		int pixel;
+		int[] color = new int[4];
+		int counter = 0;
+		int pixel, grayPixel;
 
 		for (int x = 0; x < _pictureWidth; x++) {
 			for (int y = 0; y < _pictureHeight; y++) {
-				pixel = _coloredPicture.getRGB(x, y);
+				pixel = _importedPicture.getRGB(x, y);
+				grayPixel = pixelToLuminosity(pixel);
 
-				if (pixelToLuminosity(pixel) / 51 == targetRange) {
-					lightColour[0] += (pixel >> 24) & 0xff;
-					lightColour[1] += (pixel >> 16) & 0xff;
-					lightColour[2] += (pixel >> 8) & 0xff;
-					lightColour[3] += pixel & 0xff;
-					lightColour[4]++;
+				if (grayPixel / 51 == targetRange) {
+					color[0] += (pixel >> 24) & 0xff;
+					color[1] += (pixel >> 16) & 0xff;
+					color[2] += (pixel >> 8) & 0xff;
+					color[3] += pixel & 0xff;
+
+					counter++;
 				}
 			}
 		}
 
 		for (int i = 0; i < 4; i++) {
-			lightColour[i] /= lightColour[4];
+			color[i] /= counter;
 		}
 
-		int lightPixel = (255 << 24) | (lightColour[1] << 16) | (lightColour[2] << 8) | lightColour[3];
-
-		return lightPixel;
+		return ((255 << 24) | (color[1] << 16) | (color[2] << 8) | color[3]);
 	}
 
 	private static int pixelToLuminosity(int pixel) {
@@ -163,31 +96,62 @@ public final class ColoredDitheringConvertor {
 		return (int) (.3f * r + .59f * g + .11f * b);
 	}
 
+	private static int[] rgbToHsv(int[] rgb) {
+		float r = (float) rgb[0] / 255f;
+		float g = (float) rgb[1] / 255f;
+		float b = (float) rgb[3] / 255f;
+
+		float M = Math.max(r, Math.max(g, b));
+		float m = Math.min(r, Math.min(g, b));
+		float c = M - m;
+		int s = (int) ((c / M) * 100f);
+
+		float R = (M - r) / c;
+		float G = (M - g) / c;
+		float B = (M - b) / c;
+
+		float H = 0;
+		if (M == m) {
+			H = 0;
+		} else if (M == r) {
+			H = 0f + B - G;
+		} else if (M == g) {
+			H = 2f + R - B;
+		} else if (M == b) {
+			H = 4f + G - R;
+		}
+		
+		int h = (int) (((H / 6f) % 1 ) * 360f);
+		int v = (int) (M * 100f);
+		
+		return new int[] {h, s, v};
+	}
+
+	private static void mainLoop() {
+		int pixel;
+
+		for (int x = 0; x < _pictureWidth; x++) {
+			for (int y = 0; y < _pictureHeight; y++) {
+				pixel = _importedPicture.getRGB(x, y);
+				pixel = pixelToLuminosity(pixel);
+				setPixelValue(x, y, pixel);
+			}
+		}
+	}
+
 	private static void setPixelValue(int x, int y, int avg) {
 		switch (avg / 51) {
 		case 0:
 			_exportedPicture.setRGB(x, y, _blackPixel);
 			break;
 		case 1:
-			if (x % 2 == 0 || y % 2 == 0) {
-				_exportedPicture.setRGB(x, y, _darkBlackPixel);
-			} else {
-				_exportedPicture.setRGB(x, y, _darkWhitePixel);
-			}
+			_exportedPicture.setRGB(x, y, _lightPixel);
 			break;
 		case 2:
-			if ((x + y) % 2 == 0) {
-				_exportedPicture.setRGB(x, y, _mediumBlackPixel);
-			} else {
-				_exportedPicture.setRGB(x, y, _mediumWhitePixel);
-			}
+			_exportedPicture.setRGB(x, y, _mediumPixel);
 			break;
 		case 3:
-			if (x % 2 == 0 && y % 2 == 0) {
-				_exportedPicture.setRGB(x, y, _lightBlackPixel);
-			} else {
-				_exportedPicture.setRGB(x, y, _lightWhitePixel);
-			}
+			_exportedPicture.setRGB(x, y, _darkPixel);
 			break;
 		default:
 			_exportedPicture.setRGB(x, y, _whitePixel);
