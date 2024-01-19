22dcb7f38b33d4e201b251762408739e2e1ab729
Riggor-Morthis <roc.syl@outlook.com>
2024-01-19 09:15:50 +0100

    Revert "Working alternative palette"
    This reverts commit 6cba0282b4db01af08e857e92718fccee78d4c85.

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,6 @@ package main;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
-import picture.AlternativePaletteDithering;
 import picture.ColoredDitheringConvertor;
 import picture.DitheringConvertor;
 import picture.GrayscaleConvertor;
@@ -17,11 +16,17 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedImage colouredDithering;
 		BufferedImage joconde = PictureImporter.importPicture("pictures/joconde.png");
+		
+		colouredDithering = PaletteDitheringConvertor.convert(joconde, 256, 2);
+		PictureExporter.exportPicture(colouredDithering, "pictures/jocondeTwo.png", 5);
 
 		colouredDithering = PaletteDitheringConvertor.convert(joconde, 256, 4);
-		PictureExporter.exportPicture(colouredDithering, "pictures/jocondePalette.png", 5);
-		
-		colouredDithering = AlternativePaletteDithering.convert(joconde, 256, 4);
-		PictureExporter.exportPicture(colouredDithering, "pictures/jocondeAlternative.png", 5);
+		PictureExporter.exportPicture(colouredDithering, "pictures/jocondeFour.png", 5);
+
+		colouredDithering = PaletteDitheringConvertor.convert(joconde, 256, 8);
+		PictureExporter.exportPicture(colouredDithering, "pictures/jocondeHeight.png", 5);
+
+		colouredDithering = PaletteDitheringConvertor.convert(joconde, 256, 16);
+		PictureExporter.exportPicture(colouredDithering, "pictures/jocondeSixteen.png", 5);
 	}
 }
