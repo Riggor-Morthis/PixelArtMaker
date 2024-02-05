dcc383a46f7179008090b3420039ee8ff065fb8d
Riggor-Morthis <roc.syl@outlook.com>
2024-02-05 20:32:11 +0100

    Alternative dithering colour palette

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import java.io.IOException;
 
 import scripts.DitheringColored;
+import scripts.DitheringColoredAlternative;
 import scripts.DitheringGrayscale;
 import scripts.DitheringPalette;
 import scripts.ImagePackage;
@@ -13,6 +14,7 @@ public class Main {
         ImagePackage joconde = new ImagePackage("pictures/joconde.png", 128, 4);
         DitheringGrayscale.create(joconde, "pictures/jocondegreyscale.png", 5);
         DitheringColored.create(joconde, "pictures/jocondecolored.png", 5);
+        DitheringColoredAlternative.create(joconde, "pictures/jocondecoloredalternative.png", 5);
         DitheringPalette.create(joconde, "pictures/jocondepalette.png", 5, "pictures/palette_mastersystem.png");
     }
 }
---
+++ src/scripts/DitheringColoredAlternative.java
@@ -0,0 +1,80 @@
+package scripts;
+
+import java.awt.image.BufferedImage;
+
+public class DitheringColoredAlternative {
+
+    /* VARIABLES */
+
+    private static ImagePackage imagePackage;
+    private static BufferedImage finalImage;
+    private static int[] palette;
+
+    /* CONSTRUCTORS */
+    private DitheringColoredAlternative() {
+    }
+
+    /* PUBLIC METHODS */
+
+    public static void create(ImagePackage iP, String path, int scaleFactor) {
+        imagePackage = iP;
+        createImage();
+        Utils.exportImage(path, scaleFactor, finalImage, iP);
+    }
+
+    /* PRIVATE METHODS */
+
+    private static void createImage() {
+        finalImage = new BufferedImage(imagePackage.getPixelArtWidth(), imagePackage.getPixelArtHeight(),
+                BufferedImage.TYPE_INT_ARGB);
+        createPalette();
+
+        for (int x = 0; x < finalImage.getWidth(); x++) {
+            for (int y = 0; y < finalImage.getHeight(); y++) {
+                finalImage = Utils.setColor(x, y, imagePackage.getLuminosity()[x][y], finalImage, palette);
+            }
+        }
+    }
+
+    private static void createPalette() {
+        palette = new int[imagePackage.getPaletteSize()];
+
+        for (int i = 0; i < imagePackage.getPaletteSize(); i++) {
+            palette[i] = getPixelsAverage(i * 2, imagePackage);
+        }
+    }
+
+    private static int getPixelsAverage(int luminosity, ImagePackage imagePackage) {
+        float[] pixelValues = new float[4];
+        int[] pixelBuffer;
+
+        for (int x = 0; x < imagePackage.getPixelArtWidth(); x++) {
+            for (int y = 0; y < imagePackage.getPixelArtHeight(); y++) {
+
+                if (imagePackage.getLuminosity()[x][y] == luminosity
+                        || imagePackage.getLuminosity()[x][y] == luminosity - 1
+                        || imagePackage.getLuminosity()[x][y] == luminosity + 1) {
+                    pixelBuffer = extractPixelRgb(imagePackage.getBaseImage().getRGB(x, y));
+                    for (int i = 0; i < 3; i++) {
+                        pixelValues[i] += pixelBuffer[i];
+                    }
+                    pixelValues[3]++;
+                }
+            }
+        }
+
+        return getAverage(pixelValues);
+    }
+
+    private static int[] extractPixelRgb(int pixel) {
+        return new int[] { (pixel >> 16) & 0xff, (pixel >> 8) & 0xff, (pixel) & 0xff };
+    }
+
+    private static int getAverage(float[] pixelValues) {
+        int r = Math.round(pixelValues[0] / pixelValues[3]);
+        int g = Math.round(pixelValues[1] / pixelValues[3]);
+        int b = Math.round(pixelValues[2] / pixelValues[3]);
+
+        return 255 << 24 | r << 16 | g << 8 | b;
+    }
+}
